7ad0d4fdc3b5809f1d266051d097e93849d0909d
Ivan <way8ne8@gmail.com>
2021-08-28 12:48:32 +0300

    add test basket

--- pages/locators.py
+++ pages/locators.py
@@ -7,3 +7,12 @@ class MainPageLocators():
 class LoginPageLocators():
     REG_LINK = (By.CSS_SELECTOR, "#register_form")
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_form")
+
+class ProductPageLocators():
+    ADD_LINK = (By.CSS_SELECTOR, "button.btn-add-to-basket")
+    ADD_SUCC_MESSAGE = (By.CSS_SELECTOR, ".alert-success")
+    BASKET_PRICE_MESSAGE = (By.XPATH, "//*[@id='messages']/div[3]/div")
+    NAME_PRODUCT = (By.CSS_SELECTOR, ".col-sm-6.product_main h1")
+    NAME_IN_BASKET = (By.XPATH, "//*[@id='messages']/div[1]/div/strong")
+    VALUE_ON_PAGE =  (By.CSS_SELECTOR, "p.price_color")
+    VALUE_IN_MESSEGE = (By.XPATH, "//*[@id='messages']/div[3]/div/p[1]/strong")
---
+++ pages/product_page.py
@@ -0,0 +1,17 @@
+from .base_page import BasePage, solve_quiz_and_get_code
+from .locators import ProductPageLocators
+
+class ProductPage(BasePage):
+    def add_to_basket(self):
+        self.browser.find_element(*ProductPageLocators.ADD_LINK).click()
+        solve_quiz_and_get_code(self)
+        self.check_messages()
+
+
+    def check_messages(self):
+        assert self.is_element_present(*ProductPageLocators.ADD_SUCC_MESSAGE), "Нет сообщения об успешном добавлении"
+        assert self.is_element_present(*ProductPageLocators.BASKET_PRICE_MESSAGE), "Нет сообщения о стоимости в корзине"
+        assert self.browser.find_element(*ProductPageLocators.NAME_IN_BASKET).text == self.browser.find_element(*ProductPageLocators.NAME_PRODUCT).text, \
+            "Название на странице и в корзине не совпадают"
+        assert self.browser.find_element(*ProductPageLocators.VALUE_IN_MESSEGE).text == self.browser.find_element(*ProductPageLocators.VALUE_ON_PAGE).text, \
+            "Неверная цена в сообщении"
